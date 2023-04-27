fbc209bd3dcc0edc2446b2918e26eb42849c3ae8
Ricardo Flores <rik.fm1809@gmail.com>
2023-04-27 02:04:37 -0500

    Revert "Add fastify"
    This reverts commit c1789caf57aa3557fa2ffb18d01444aaf770b42f.

--- NOTES.md
+++
@@ -1,2 +0,0 @@
-## Registrar schema.prisma si ya tienes la carpeta prisma generada
-npx prisma generate
--- src/modules/users/users.service.ts
+++
@@ -1,5 +0,0 @@
-import prisma from "../../utils/prisma";
-
-export async function getUsers() {
-   return prisma.user.findMany()
-}
