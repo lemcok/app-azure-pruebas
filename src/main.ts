4290fd77eda0153f621e22640695c311da0d0557
Ricardo Flores <rik.fm1809@gmail.com>
2023-04-27 03:53:51 -0500

    Revert "con fastity""
    This reverts commit fbc209bd3dcc0edc2446b2918e26eb42849c3ae8.

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { PORT } from './config'
+import { buildApp } from "./app";
+
+const server = buildApp()
+
+async function main (){
+   try {
+      await server.listen({port:Number(process.env.PORT)})
+      console.log(`Server running at http://localhost:${process.env.PORT}`);
+   } catch (err) {
+      console.log( err );
+      process.exit(1)
+   }
+}
+
+main();
---
+++ src/modules/users/users.schemas.ts
@@ -0,0 +1,11 @@
+export type CreateUserDTO = {
+    email: string;
+    name: string;
+    password: string;
+}
+
+export type UpdateUserDTO = {
+    email: string;
+    name: string;
+    password: string;
+}
